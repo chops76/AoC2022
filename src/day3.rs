dedf28d701b98e63da3bbef792631b04a7214e19
Dan Chopskie <dchopskie@gmail.com>
2022-12-03 07:59:14 -0600

    Improvements???

--- src/day3.rs
+++ src/day3.rs
@@ -10,41 +10,33 @@ fn parse_input(filename: &str) -> Input {
     BufReader::new(f).lines().flatten().collect()
 }
 
-fn parse_sacks(sack_str: &str) -> (HashSet<char>, HashSet<char>) {
-    let left = sack_str[..sack_str.len()/2].chars().collect::<HashSet<char>>();
-    let right = sack_str[sack_str.len()/2..].chars().collect::<HashSet<char>>();
-    (left,right)
+fn parse_sacks(sack_str: &str) -> Vec<HashSet<char>> {
+    let half = sack_str.len()/2;
+    [0,half].iter().map(|&v| sack_str[v..v+half].chars().collect::<HashSet<char>>()).collect()
 }
 
 fn calc_value(c: &char) -> usize {
-    let mut score = *c as u32;
-    if score >= 97 {
-        score -= 96;
-    } else {
-        score -= 38;
+    let score = *c as u32;
+    (score - if score >= 97 { 96 } else { 38 }) as usize    
+}
+
+fn find_score(sacks: &[HashSet<char>]) -> usize {
+    let mut i = sacks.iter();
+    let mut s:HashSet<char> = i.next().unwrap().clone();
+    for sack in i {
+        s = s.intersection(&sack).copied().collect();
     }
-    score as usize    
-}
-
-fn find_score1(l: &HashSet<char>, r: &HashSet<char>) -> usize {
-    let common:Vec<_> = l.intersection(&r).collect();
-    calc_value(common[0])
-}
-
-fn find_score2(sacks: &[HashSet<char>]) -> usize {
-    let i1:HashSet<_> = sacks[0].intersection(&sacks[1]).map(|c| *c).collect();
-    let i2:Vec<_> = i1.intersection(&sacks[2]).collect();
-    calc_value(i2[0])
+    calc_value(s.iter().next().unwrap())
 }
 
 fn part1(sacks: &Input) -> usize {
     sacks.iter().map(|s| parse_sacks(s))
-                .map(|(l, r)| find_score1(&l, &r)).sum()
+                .map(|s| find_score(&s)).sum()
 }
 
 fn part2(sacks: &Input) -> usize {
    let parsed: Vec<_> = sacks.iter().map(|s| s.chars().collect::<HashSet<char>>()).collect();
-   ((0..parsed.len()).step_by(3)).map(|v| find_score2(&parsed[v..v+3])).sum()
+   ((0..parsed.len()).step_by(3)).map(|v| find_score(&parsed[v..v+3])).sum()
 }
 
 pub fn main() {
