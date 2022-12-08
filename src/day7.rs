d310b13d343cfcf362f44dde11e2011bc170c6af
Dan Chopskie <dchopskie@gmail.com>
2022-12-07 22:42:32 -0600

    Small cleanup

--- src/day7.rs
+++ src/day7.rs
@@ -3,8 +3,8 @@ use std::fs::File;
 use std::time::Instant;
 
 type Input = Vec<String>;
-#[derive(Debug)]
 
+#[derive(Debug)]
 struct FileEntry {
     name: String,
     size: usize
@@ -34,7 +34,7 @@ fn calc_size(dir_num: usize, dirs: &Vec<Directory>, files: &Vec<FileEntry>) -> u
     sum
 }
 
-fn part1(terminal: &Input) -> (usize,usize) {
+fn create_tree(terminal: &Input) -> (Vec<Directory>,Vec<FileEntry>) {
     let mut dirs: Vec<Directory> = Vec::new();
     let mut files: Vec<FileEntry> = Vec::new();
     dirs.push(Directory { name: "/".to_string(), parent: 0, files: Vec::new(), subdirs: Vec::new() });
@@ -44,66 +44,66 @@ fn part1(terminal: &Input) -> (usize,usize) {
     let mut last_file = 0;
     while cur_line < terminal.len() {
         let spl = terminal[cur_line].split_ascii_whitespace().collect::<Vec<&str>>();
-        if spl[1] == "cd" {
-            if spl[2] == "/" {
-                cur_dir = 0;
-            } else if spl[2] == ".." {
-                cur_dir = dirs[cur_dir].parent;
-            } else {
-                for d in &dirs[cur_dir].subdirs {
-                    if dirs[*d].name == spl[2] {
-                        cur_dir = *d;
-                        break;
+        if spl[0] == "$" {
+            if spl[1] == "cd" {
+                if spl[2] == "/" {
+                    cur_dir = 0;
+                } else if spl[2] == ".." {
+                    cur_dir = dirs[cur_dir].parent;
+                } else {
+                    for d in &dirs[cur_dir].subdirs {
+                        if dirs[*d].name == spl[2] {
+                            cur_dir = *d;
+                            break;
+                        }
                     }
                 }
             }
-        } else if spl[1] == "ls" {
-            loop {
-                cur_line += 1;
-                if cur_line >= terminal.len() {
-                    break;
-                }
-                let spl = terminal[cur_line].split_ascii_whitespace().collect::<Vec<&str>>();
-                if spl[0] == "$" {
-                    break;
-                }
-                if spl[0] == "dir" { 
-                    let new_dir = Directory { name: spl[1].to_string(), parent: cur_dir, 
-                                                         files: Vec::new(), subdirs: Vec::new()};
-                    dirs.push(new_dir);
-                    dirs[cur_dir].subdirs.push( last_dir );
-                    last_dir += 1;
-                } else {
-                    let new_entry = FileEntry { name: spl[1].to_string(), size: spl[0].parse().unwrap() };
-                    files.push( new_entry );
-                    dirs[cur_dir].files.push(last_file);
-                    last_file += 1;
-                }
+        } else {
+            if spl[0] == "dir" { 
+                let new_dir = Directory { name: spl[1].to_string(), parent: cur_dir, 
+                                                        files: Vec::new(), subdirs: Vec::new()};
+                dirs.push(new_dir);
+                dirs[cur_dir].subdirs.push( last_dir );
+                last_dir += 1;
+            } else {
+                let new_entry = FileEntry { name: spl[1].to_string(), size: spl[0].parse().unwrap() };
+                files.push( new_entry );
+                dirs[cur_dir].files.push(last_file);
+                last_file += 1;
             }
-            cur_line -= 1;
         }
         cur_line += 1;
     }
+    (dirs,files)
+}
 
-    let mut part1_total = 0;
-    for d in 0..last_dir {
+fn part1(terminal: &Input) -> usize {
+    let (dirs,files) = create_tree(terminal);
+    let mut total = 0;
+    for d in 0..dirs.len() {
         let sum = calc_size(d, &dirs, &files);
         if sum <= 100000 {
-            part1_total += sum;
+            total += sum;
         }
-    }
+    } 
+    total
+}
+
+fn part2(terminal: &Input) -> usize {
+    let (dirs,files) = create_tree(terminal);
 
     let root_size = calc_size(0, &dirs, &files);
     let free = 70000000 - root_size;
     let needed = 30000000 - free;
     let mut best = root_size;
-    for d in 0..last_dir {
+    for d in 0..dirs.len() {
         let sum = calc_size(d, &dirs, &files);
         if sum > needed && sum < best {
             best = sum;
         }
     }    
-    (part1_total, best)
+    best
 }
 
 pub fn main() {
@@ -112,8 +112,14 @@ pub fn main() {
     let p1_timer = Instant::now();
     let p1_result = part1(&terminal);
     let p1_time = p1_timer.elapsed();
-    println!("Part 1: {}, Part 2: {}", p1_result.0, p1_result.1);
+    println!("Part 1: {}", p1_result);
     println!("Time: {:?}", p1_time);
+
+    let p2_timer = Instant::now();
+    let p2_result = part2(&terminal);
+    let p2_time = p2_timer.elapsed();
+    println!("Part 2: {}", p2_result);
+    println!("Part 2 Time: {:?}", p2_time); 
 }
 
 #[cfg(test)]
@@ -124,7 +130,13 @@ mod tests {
     #[test]
     fn day7_test1() {
         let terminal = parse_input("./input/day7/test.txt");
-        assert_eq!(part1(&terminal), (95437,24933642));
+        assert_eq!(part1(&terminal), 95437);
+	}
+
+    #[test]
+    fn day7_test2() {
+        let terminal = parse_input("./input/day7/test.txt");
+        assert_eq!(part2(&terminal), 24933642);
 	}
 
 }
