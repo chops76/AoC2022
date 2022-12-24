e583165d870003b14235977774f8b03ab9af2c12
Dan Chopskie <dchopskie@gmail.com>
2022-12-24 00:59:35 -0600

    Day 24

--- src/day24.rs
+++ src/day24.rs
@@ -23,41 +23,14 @@ fn get_blizzards(grid: &Vec<Vec<char>>) -> HashSet<(usize,usize,char)> {
     blizzards
 }
 
-fn move_blizzards(blizzards: &HashSet<(usize,usize,char)>, max_x: usize, max_y: usize) -> HashSet<(usize,usize,char)> {
-    let mut new_bliz = HashSet::new();
-    for (x,y,dir) in blizzards {
-        match dir {
-            '^' => { 
-                let new_y = if *y == 1 { max_y } else { y - 1 };
-                new_bliz.insert((*x,new_y,'^'));
-            },
-            'v' => {
-                let new_y = if *y == max_y { 1 } else { y + 1 };
-                new_bliz.insert((*x, new_y, 'v'));
-            },
-            '>' => {
-                let new_x = if *x == max_x { 1 } else { x + 1 };
-                new_bliz.insert((new_x, *y, '>'));
-            },
-            '<' => {
-                let new_x = if *x == 1 { max_x } else { x - 1 };
-                new_bliz.insert((new_x, *y, '<'));
-            },
-            _ => { println!("Illegal Direction"); }
-        }
-    }
-
-    new_bliz
-}
-
 fn part1(grid: &Vec<Vec<char>>) -> i64 {
     let blizzards = get_blizzards(grid);
     let mut queue = VecDeque::new();
     let mut seen = HashSet::new();
     let max_x = grid[0].len() - 2;
     let max_y = grid.len() - 2;
-    queue.push_back((1, 0, blizzards, 0));
-    while let Some((x,y,blizzards,steps)) = queue.pop_front() {
+    queue.push_back((1, 0, 0));
+    while let Some((x,y,steps)) = queue.pop_front() {
         if x == grid[y].len() - 2 && y == grid.len() - 1 {
             return steps;
         }
@@ -65,23 +38,49 @@ fn part1(grid: &Vec<Vec<char>>) -> i64 {
             continue;
         }
         seen.insert((x,y,steps));
-        if blizzards.contains(&(x,y,'^')) || blizzards.contains(&(x,y,'v')) 
-           || blizzards.contains(&(x,y,'<')) || blizzards.contains(&(x,y,'>')) {
+        let mut collision = false;
+        for b in &blizzards {
+            let mut new_x = b.0;
+            let mut new_y = b.1;
+            match b.2 {
+                '<' => {
+                    let offset = steps as usize % max_x;
+                    new_x = ((new_x - 1) + max_x - offset) % max_x + 1;
+                },
+                '>' => {
+                    let offset = steps as usize % max_x;
+                    new_x = ((new_x - 1) + offset) % max_x + 1;
+                },
+                '^' => {
+                    let offset = steps as usize % max_y;
+                    new_y = ((new_y - 1) + max_y - offset) % max_y + 1;
+                },
+                'v' => {
+                    let offset = steps as usize % max_y;
+                    new_y = ((new_y - 1) + offset) % max_y + 1;
+                },
+                _ => { println!("Illegal direction"); }
+            }
+            if x == new_x && y == new_y {
+                collision = true;
+                break;
+            }
+        }
+        if collision {
             continue;
         }
-        let new_bliz = move_blizzards(&blizzards, max_x, max_y);
-        queue.push_back((x,y,new_bliz.clone(),steps+1));
+        queue.push_back((x,y,steps+1));
         if y < max_y || x == max_x {
-            queue.push_back((x,y+1,new_bliz.clone(), steps+1));
+            queue.push_back((x,y+1,steps+1));
         }
         if y > 1 {
-            queue.push_back((x,y-1,new_bliz.clone(), steps+1));
+            queue.push_back((x,y-1,steps+1));
         }
         if x > 1 {
-            queue.push_back((x-1,y,new_bliz.clone(),steps+1));
+            queue.push_back((x-1,y,steps+1));
         }
         if x < max_x && y != 0 {
-            queue.push_back((x+1,y,new_bliz.clone(), steps+1));
+            queue.push_back((x+1,y,steps+1));
         }
     }
     0
@@ -93,9 +92,8 @@ fn part2(grid: &Vec<Vec<char>>) -> usize {
     let mut seen = HashSet::new();
     let max_x = grid[0].len() - 2;
     let max_y = grid.len() - 2;
-    queue.push_back((1, 0, blizzards, 0, false, false));
-    while let Some((x,y,blizzards,steps, goal_seen, start_seen)) = queue.pop_front() {
-        //println!("Steps = {}, goal_seen = {}, start_seen = {}", steps, goal_seen, start_seen);
+    queue.push_back((1, 0, 0, false, false));
+    while let Some((x,y,steps, goal_seen, start_seen)) = queue.pop_front() {
         let mut new_goal_seen = goal_seen;
         let mut new_start_seen = start_seen;
         if seen.contains(&(x, y, steps,goal_seen,start_seen)) {
@@ -111,24 +109,50 @@ fn part2(grid: &Vec<Vec<char>>) -> usize {
         if x == 1 && y == 0 && new_goal_seen {
             new_start_seen = true;
         }
-
-        if blizzards.contains(&(x,y,'^')) || blizzards.contains(&(x,y,'v')) 
-           || blizzards.contains(&(x,y,'<')) || blizzards.contains(&(x,y,'>')) {
+        let mut collision = false;
+        for b in &blizzards {
+            let mut new_x = b.0;
+            let mut new_y = b.1;
+            match b.2 {
+                '<' => {
+                    let offset = steps as usize % max_x;
+                    new_x = ((new_x - 1) + max_x - offset) % max_x + 1;
+                },
+                '>' => {
+                    let offset = steps as usize % max_x;
+                    new_x = ((new_x - 1) + offset) % max_x + 1;
+                },
+                '^' => {
+                    let offset = steps as usize % max_y;
+                    new_y = ((new_y - 1) + max_y - offset) % max_y + 1;
+                },
+                'v' => {
+                    let offset = steps as usize % max_y;
+                    new_y = ((new_y - 1) + offset) % max_y + 1;
+                },
+                _ => { println!("Illegal direction"); }
+            }
+            if x == new_x && y == new_y {
+                collision = true;
+                break;
+            }
+        }
+        if collision {
             continue;
         }
-        let new_bliz = move_blizzards(&blizzards, max_x, max_y);
-        queue.push_back((x,y,new_bliz.clone(),steps+1,new_goal_seen,new_start_seen));
+
+        queue.push_back((x,y,steps+1,new_goal_seen,new_start_seen));
         if y < max_y || (x == max_x && y == max_y){
-            queue.push_back((x,y+1,new_bliz.clone(), steps+1,new_goal_seen,new_start_seen));
+            queue.push_back((x,y+1, steps+1,new_goal_seen,new_start_seen));
         }
         if y > 1 || (y == 1 && x == 1) {
-            queue.push_back((x,y-1,new_bliz.clone(), steps+1,new_goal_seen,new_start_seen));
+            queue.push_back((x,y-1, steps+1,new_goal_seen,new_start_seen));
         }
         if x > 1 && y <= max_y {
-            queue.push_back((x-1,y,new_bliz.clone(),steps+1,new_goal_seen,new_start_seen));
+            queue.push_back((x-1,y,steps+1,new_goal_seen,new_start_seen));
         }
         if x < max_x && y != 0 && y <= max_y {
-            queue.push_back((x+1,y,new_bliz.clone(), steps+1,new_goal_seen,new_start_seen));
+            queue.push_back((x+1,y, steps+1,new_goal_seen,new_start_seen));
         }
     }
     0
@@ -158,12 +182,12 @@ mod tests {
     #[test]
     fn day23_test1() {
         let inst = parse_input("./input/day24/test.txt");
-        assert_eq!(part1(&inst), 110);
+        assert_eq!(part1(&inst), 18);
 	}
 
     #[test]
     fn day23_test2() {
         let inst = parse_input("./input/day24/test.txt");
-        assert_eq!(part2(&inst), 20);
+        assert_eq!(part2(&inst), 54);
 	}
 }
